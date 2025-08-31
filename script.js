4a6687f827e0018d6a97d6cd56c04bcc25e4196a
Jasur402 <djasur.66@hotmail.com>
2025-08-31 13:53:31 +0200

    gg

---
+++ script.js
@@ -0,0 +1,119 @@
+/// 1 ////
+/*  const get = fetch('https://jsonplaceholder.typicode.com/posts/1',{
+  method: 'GET'
+})
+
+get
+.then((response) => {
+   if (response) {
+   return response.json()
+ }else{
+   throw new Error("Ошибка запроса");
+ }
+})
+.then((json) => {
+console.log(json.title)
+})
+.catch((err) => {
+   console.log(err);
+})
+   */
+
+///// 2 ////
+/* const post = fetch('https://jsonplaceholder.typicode.com/posts',{
+  method: 'POST',
+  body:JSON.stringify({
+    title: "My homework post",
+    body: "This is a test post",
+    userId: 1
+  })
+})
+
+post
+.then((response) => {
+  if (response.ok) {
+     return response.json()
+  }else{
+    throw new Error("Ошибка создания поста");
+  }
+})
+.then((json) => {
+console.log(json.id)
+})
+.catch((err) => {
+  console.log(err);
+  
+})  */
+/// 3 ///
+
+ /*  const put = fetch('https://jsonplaceholder.typicode.com/posts/1',{
+  method: 'PUT',
+  headers: {'Content-Type': 'application/json; charset=UTF-8'},
+  body: JSON.stringify({
+  id: 1,
+  title: "Updated title",
+  body: "Updated body",
+  userId: 1
+})
+})
+
+put
+.then((response) => {
+  if (response.ok) {
+    return response.json()
+  }else{
+    throw new Error("Ошибка обновления поста");
+  }
+})
+.then((json) => {
+ console.log(json.title)
+})
+.catch((err) => {
+  console.log(err)
+})
+ */
+
+/// 4 ///
+
+/*  const patch = fetch('https://jsonplaceholder.typicode.com/posts/1',{
+  method: 'PATCH',
+  headers: {'Content-Type': 'application/json; charset=UTF-8'},
+  body: JSON.stringify({
+  title: "PATCHED title",
+})
+})
+
+patch
+.then((response) => {
+  if (response.ok) {
+    return response.json()
+  }else{
+    throw new Error("Ошибка изменения поста");
+  }
+})
+.then((json) => {
+ console.log(json.title)
+})
+.catch((err) => {
+  console.log(err)
+})  */
+
+////  5 ////
+
+/*  const delet = fetch('https://jsonplaceholder.typicode.com/posts/1',{
+   method: 'DELETE',
+})
+
+delet
+.then((response) => {
+  if (response.ok) {
+    console.log("Пост удалён")
+  }else{
+    throw new Error("Ошибка удаления поста");
+  }
+})
+.catch((err) => {
+  console.log(err)}) */
+
+
+
